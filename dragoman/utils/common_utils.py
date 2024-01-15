294cdf951c82e5575824a68400b1c21f2c3e10ee
dimitrionian-II <dmitry.bezlutzky@yandex.ru>
2024-01-15 19:31:21 +0600

    Get translatable fields

--- dragoman/utils/common_utils.py
+++ dragoman/utils/common_utils.py
@@ -8,21 +8,24 @@ from dragoman.utils.provider_utils import TranslationProvider
 
 class ModelUtils:
     @classmethod
-    def get_models(cls) -> List[Type[Model]]:
+    def get_translatable_models(cls) -> List[Type[Model]]:
         all_models = [model for model in django.apps.apps.get_models()]
         tr_keys = list(settings.TRANSLATION_DISPATCHER["models"].keys())
 
         return [model for model in all_models if model._meta.object_name in tr_keys]
 
     @classmethod
-    def collect_models(cls):
-        models = ModelUtils.get_models()
+    def get_translatable_fields(cls, model: Type[Model]) -> List[str]:
         tr_dispatcher = settings.TRANSLATION_DISPATCHER
+        return tr_dispatcher["models"][model._meta.object_name][settings.TRANSLATE_FIELDS_NAME]
+
+    @classmethod
+    def collect_models(cls):
+        models = ModelUtils.get_translatable_models()
 
         for model in models:
             for instance in model.objects.all():
-                translate_fields = tr_dispatcher["models"][instance._meta.object_name][settings.TRANSLATE_FIELDS_NAME]
-                ModelUtils.update_source_translation_by_instance(instance, translate_fields)
+                ModelUtils.update_source_translation_by_instance(instance, cls.get_translatable_fields(instance))
 
     @classmethod
     def update_source_translation_by_instance(cls, instance: Model, translate_fields: List[str]):
